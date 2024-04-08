5ea10f4c0c48d2a99db6d50bdd644ec0cd882536
Lane Lu <llu@deltasoft365.com>
2024-04-08 20:58:53 +0800

    feat: test api and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Form, FormBuilder } from '@formio/react';
 import './App.css';
 
 function App() {
+
+  const form = {
+    title: 'Example Form',
+    display: 'form',
+    components: [
+      {
+        label: 'Name',
+        key: 'name',
+        type: 'textfield',
+      },
+      {
+        label: 'Email',
+        key: 'email',
+        type: 'email',
+      },
+      {
+        label: 'Save',
+        key: 'save',
+        type: 'button',
+        action: 'submit',
+      },
+    ],
+  };
+
+  const data = {
+    name: 'Lane',
+    email: 'lane.cn@gmail.com',
+  }
+
+  const onSubmitHandler = (submission: any) => {
+    console.table(submission.data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2>{form.title}</h2>
+      <FormBuilder
+        form={form}
+        onChange={(props: any) => console.log(props)}
+      />
+      <Form
+        // src={'https://example.form.io/example'}
+        form={form} 
+        submission={{data: data}} 
+        onSubmit={(submission: any) => onSubmitHandler(submission)} 
+      />
     </div>
   );
 }
